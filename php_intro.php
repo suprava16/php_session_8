2e55a883a75d29c3cf407d5b364a4f72f895d62c
suprava padhi <supravapadhi92@gmail.com>
2022-01-08 20:46:53 +0530

    php code

---
+++ php_intro.php
@@ -0,0 +1,26 @@
+<?php
+echo "welcome everyone...";
+echo "<h1> html code within php tags</h1>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    h1{
+      color:red;
+    }
+  </style>
+</head>
+<body>
+  <h1>Heading</h1>
+  <?php echo "something";
+  <script>
+    document.write("hello...........")
+  </script>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,27 @@
+<?php
+$a=30;
+$b=55.9;
+$c="suprava";
+$d='supriya';
+$e=false;
+
+echo $a;
+echo "<br/>";
+echo $b ."<br/>";
+echo "value of c=",$c ."<br/>";
+echo "value of d=$d"."<br/>";
+echo 'value of d=$d';
+echo "welcome to 'Nextstacks'"."<br/>";
+
+//operators
+// = , == ,===
+$p=10;
+$q="10";
+
+echo "value=",$p==$q;//true
+echo "value=",$p===$q;//false
+
+
+
+
+?>
