74ab3d48148439b4ed246ebc014c59a53824a8f1
suprava padhi <supravapadhi92@gmail.com>
2022-01-17 18:39:12 +0530

    database connection

--- get&post/get_post.php
+++ get&post/get_post.php
@@ -7,13 +7,26 @@
 // echo $_GET['user_password'];
 
 if(isset($_POST['login'])){
-  $username=$_POST['username'];
-  echo $username;
-  echo $_POST['user_email'];
-  echo $_POST['user_password'];
+  // $username=$_POST['username'];
+  // echo $username;
+  $user_email=$_POST['user_email'];
+  $password=$_POST['user_password'];
+if($user_email==="suprava@gmail.com" && $password==="sup"){
+  header("location:home.php?id=10");
+}
+else{
+  echo "something wrong... try again....!!!!!!!";
+}
+
 }
 else{
   echo "try again......";
 }
 
 ?>
+<!-- <table>
+  <tr>
+    <td><?php echo $user_email;?></td>
+  </tr>
+</table> -->
+
---
+++ get&post/home.php
@@ -0,0 +1,8 @@
+
+<h1>Welcome to home</h1>
+
+<?php
+$user_id=$_GET['id'];
+echo "id=",$user_id;
+
+?>
